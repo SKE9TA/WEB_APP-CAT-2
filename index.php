257655dd3664e450d0ceaca5d1957f119560b3a6
Gift Shasa <129386408+SKE9TA@users.noreply.github.com>
2023-11-24 10:16:41 +0300

    index.php

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Author Details Form</title>
+    <link rel="stylesheet" href="style.css">
+   
 </head>
+<div class="body">
 <body>
-    
-<form action="DBConn.php" method="post" >
-
-    <label for="AuthorFullName"> FullName:</label>
-    <input type="text" id="AuthorFullName" name= AuthorFullName required><br>
-
-    <label for="AuthorEmail"> Email:</label>
-    <input type="email" id="AuthorEmail" name= AuthorEmail required><br>
-
-    <label for="AuthorAddress">Address:</label>
-    <input type="text" id="AuthorAddress" name= AuthorAddress required><br>
-
-    <label for="AuthorBiography"> Biography:</label>
-    <input type="text" id="AuthorBiography" name= AuthorBiography required><br>
-
-    <label for="AuthorDateOfBirth"> Date of Birth:</label>
-    <input type="date" id="AuthorDateOfBirth" name= AuthorDateofBirth required><br>
-
-    <label for="AuthorSuspended"> Suspended Author:</label>
-    <input type="checkbox" id="AuthorSuspended" name= AuthorSuspended required><br>
-
-    <input type="submit" value="Submit">
-
-</form>    
+    <div class="authordetails">
+    <form action="processes/AutRegestration.php", method="POST">
+        <h2>Author Regestration Form</h2><br>
+        Author Identification Number<br>
+        <input type="number" name="Author_ID" id="AuthorID"><br>
+        Author Full Name<br/>
+        <input type="text" name="AuthorFullName"id="AuthorFullName"><br/><br/>
+        Author Email<br/>
+        <input type="email" name="AuthorEmail" id="AuthorEmail"><br/><br/>
+        Author Address<br/>
+        <input type="text" name="AuthorAddress" id="AuthorAddress"><br/><br/>
+        Author Date of Birth<br/>
+        <input type="date" name="AuthorDateOfBirth" id="AuthorDateOfBirth"><br/><br/>
+        Author Biography<br/>
+        <textarea placeholder="Write Biography here" name="AuthorBiography" id="AuthorBiography" rows="8"gti cols="80"></textarea><br/>
+        Author Suspended<br/>
+        YES/NO<br/>
+        <input type="radio" name= "AuthorStatus" id="yes" value="AuthorStatus">
+        <input type="radio" name="AuthorStatus" id="no" value="AuthorStatus"><br/>
+        <input type="submit" value="submit"><br>
+    </form>
+    </div>
+</div>
 </body>
 </html>
